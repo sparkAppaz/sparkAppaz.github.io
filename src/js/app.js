3c08b1b5e0da2ff5b7201fc5beda7de5f2fd9f03
Matt Ludwigs <matt@mattludwigs.com>
2014-12-26 20:09:32 -0700

    Started initial JS work

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,69 @@
 (function () {
 
+	var LinkFactory = (function () {
+		var linksArray = [];
+
+		return {
+			// options { linkSelector: HTMLElem, gatherLinks: bool }
+			init: function (options) {
+				var i;
+				if (options.gatherLinks) {
+					// gather links
+					this.links = document.querySelectorAll(options.linkSelector);
+					// Make new link object
+					for (i = 0; i < this.links.length; i++) {
+						linksArray.push(new Link(this.links[i]));
+					}
+				}
+			},
+
+			getLinks: function () {
+				return linksArray;
+			}
+		};
+	})();
+
+	function Link(linkElem) {
+		this.elem = linkElem;
+		this.addClickListener();
+	}
+
+	Link.prototype =  {
+		addClickListener: function () {
+			this.elem.addEventListener("click", function (event) {
+				console.log(this);
+			}.bind(this));
+		}
+	};
 
 	var router = (function () {
-
 		var routes = [];
 
-		function addRoute(url, route) {
-			var _tempObj = {
-				url: url,
-				route: route
-			};
-			
-			routes.push(_tempObj);
+		function initClickListener(routeObj) {
+			routeObj.link.addEventListener("click", function (event) {
+				console.log(routeObj);
+			});
 		}
 
 		return {
-			addRoute: addRoute,
-			routes: routes
-		};
+			// routeObj { url, view, link}
+			addRoute: function (routeObj) {
+				routes.push(routeObj);
+				initClickListener(routeObj);
+			},
 
+			getRoutes: function () {
+				return routes;
+			}
+		};
 	})();
 
 	window.router = router;
+
+	LinkFactory.init({ linkSelector: "a.link", gatherLinks: true });
+	var links = LinkFactory.getLinks();
+	console.log(links);
+
+
 
 })();
